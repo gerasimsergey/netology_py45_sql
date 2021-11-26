541db0a31ad417fcf3f2bbdedab503bc715e8ce1
Sergey <gerasim.sergey@gmail.com>
2021-11-26 14:50:52 +0200

    ДЗ по созданию таблиц - немного поправил

--- lesson_2_create.sql
+++ lesson_2_create.sql
@@ -1,3 +1,9 @@
+
+-- drop table tracks;
+-- drop table albums;
+-- drop table artists;
+
+
 create table if not exists artists (
     id serial primary key,
     nick_name varchar(10) not null,
@@ -7,13 +13,16 @@ create table if not exists artists (
 create table if not exists albums (
     id serial  primary key,
     year integer not null,
+    check (year BETWEEN 1900 AND 2100), -- ;-)
     name varchar(20) not null,
-    artist_id integer references artists(id)
+    artist_id integer references artists(id) on delete cascade
 );
 
 create table if not exists tracks (
     id serial primary key,
     name varchar(20) not null,
     durations float not null ,
-    album_id integer references albums(id)
+    album_id integer references albums(id) on delete cascade
 );
+
+
